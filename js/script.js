a73e06b8e5f3150e3db5197d7611302bde7d934f
Owl1963 <88161801+Owl1963@users.noreply.github.com>
2021-11-04 12:53:45 -0400

    starter code and quotes added
    starter code from TTH and my quotes from original start of project added

---
+++ js/script.js
@@ -0,0 +1,96 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+const quotes = [
+  {
+  quote: 'You only live once, but if you do it right, once is enough',
+  source: 'Mae West'    
+  },{
+  quote: 'Many of lifes failures are people who did not realize how close they were to success when they gave up.',
+  source: 'Thomas A. Edison'
+  },{
+  quote: 'If you want to live happy, tie it to a goal, not to people or things',
+  source: 'Albert Einstein'
+  },{
+  quote: "Your time is limited, so do not waste it living someone else's life.  Don't be trapped by dogma- which is living with the results of other people's thinking.",
+  source: 'Steve Jobs'
+  },{
+  quote: 'The whole secret of a successful life is to find out what is your destiny to do and then do it.',
+  source: 'Henry Ford'
+  },{
+  quote: 'The big lesson in life, baby, is never be scared of anyone or anything.',
+  source: 'Frank Sinatra'
+  },{
+  quote: 'Life is not a problem to be solved, but a reality to be experienced',
+  source: 'Soren Kierkegaard'
+  },{
+  quote: 'Live for each second without hesitation.',
+  source: 'Elton John'
+  },{
+  quote: 'When we do the best we can, we never know what miracle is wrought in our life or the life of another.',
+  source: 'Helen Keller'
+  },{
+  quote: 'Life is like a coin.  You can spend it any way you wish, but you only spend it once.',
+  source: 'Lillian Dickinson'
+  },{
+   quote: "Don't Panic.",
+  source: 'Douglas Adams'
+   },{
+   quote: 'Life is ten percent what happens to you and ninety percent how you respond to it.',
+   source: 'Charles Swindoll'
+  },{
+  quote: "Keep smiling, because life is a beautiful thing and there's so much to smile about.",
+  source: 'Marilyn Monroe'
+  },{
+  quote: 'You have brains in your head.  You have feet in your shoes. You can steer yourself in any direction you choose.',
+  source: 'Dr. Suess'
+  },{
+  quote: 'Life is what we make it, always has been, always will be.',
+  source: 'Grandma Moses'
+  },{
+  quote: 'The two most important days in your life are the day you are born and the day you find out why.',
+  source: 'Mark Twain'
+  },{
+  quote: 'The longer I live, the more beautiful life becomes.',
+  source: 'Frank Lloyd Wright'
+  },{
+  quote: "Don't cry because it is over, smile because it happened.",
+  source: 'Dr. Suess'
+  },{
+  quote: 'You must expect great things of yourself before you can do them.',
+  source: 'Michael Jordan'
+  },{
+  quote: 'Do not dwell on the past, do not dream of the future, concentrate the mind on the present moment.',
+  source: 'Buddha'
+  },    
+]; 
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
